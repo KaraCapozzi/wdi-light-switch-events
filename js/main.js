cf4ad358c976e308dfd020063a0f593167563819
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-10-04 16:52:49 -0400

    without comments

--- js/main.js
+++ js/main.js
@@ -14,27 +14,3 @@ if (state === 'on'){
   body.className = 'light'
   button.className = 'switch on'}
 });
-
-
-
-// var Button = function(element) {
-//   // |this| is a newly created object
-//   this.switch = 'Switch On';
-//   this.handleEvent = function(on) {
-//     console.log(this.switch);
-//     switch(event.type) {
-//       case 'click':
-//         break;
-//       case 'dblclick':
-//         break;
-//     }
-//   };
-//
-//
-//   element.addEventListener('click', this, false);
-//   element.addEventListener('dblclick', this, true);
-//
-//   element.removeEventListener('click', this, false);
-//   element.removeEventListener('dblclick', this, false);
-// }
-// var s = new Something(document.body);
